bfcca9a1de3cb12a2bb99d485c53b00a1435e437
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2021-11-28 15:40:36 +0530

    who we are

--- templates/sections/home-whoweare.php
+++ templates/sections/home-whoweare.php
@@ -1,4 +1,7 @@
 <?php $pods_settings = pods('website_settings'); ?>
 <section id="section-who-we-are" class="who_we_are_bg">
-
+    <h3 class="text-center">What we stand for</h3>
+    <div class="d-flex">
+        
+    </div>
 </section>
