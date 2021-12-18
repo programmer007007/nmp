2a16cdd9ddc4bd77f08a6f1ebfed451bdfc6850b
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2021-12-18 14:56:54 +0530

    dynamic slider

--- templates/sections/home-welcomemsg.php
+++ templates/sections/home-welcomemsg.php
@@ -1,4 +1,5 @@
 <?php $pods_settings = pods('website_settings'); ?>
+<?php $content = get_page_by_path('welcome-content', OBJECT, 'page'); ?>
 <section id="section-welcome">
     <div class="welcome_wrapper">
         <div class="welcome_container">
@@ -8,18 +9,12 @@
                         <!-- Additional required wrapper -->
                         <div class="swiper-wrapper">
                             <!-- Slides -->
+                            <?php $images = explode(" ",$pods_settings->display("welcome_images"));?>
+                            <?php foreach($images as $image){?>
                             <div class="swiper-slide">
-                                <img src="http://atmiyaproperties.com/wp-content/uploads/2017/08/a1.jpg" />
+                                <img src="<?php echo $image;?>" />
                             </div>
-                            <div class="swiper-slide">
-                                <img src="http://atmiyaproperties.com/wp-content/uploads/2017/08/a2.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="http://atmiyaproperties.com/wp-content/uploads/2017/08/a3.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="http://atmiyaproperties.com/wp-content/uploads/2017/08/a4.jpg" />
-                            </div>
+                            <?php } ?>
                         </div>
                         <div class="swiper-pagination"></div>
                         <!-- If we need pagination -->
@@ -29,16 +24,13 @@
                 </div>
                 <div class="col-md-8 col-12 width_tab_100">
                     <div class="pt-3 welcome_content_holder">
+
                         <h3 class="welcome_txt">WELCOME TO <span class="cmp_name"><?php echo strtoupper($pods_settings->display("company_name")); ?></span></h3>
                         <div class="welcome_dy_txt_holder">
                             <?php
                             $query = get_post($content->ID);
                             $content = apply_filters('the_content', $query->post_content);
-                            if(is_front_page()) {
-                                echo limitText($content, 1200, get_permalink($query->ID), true);
-                            }else{
-                                echo $content;
-                            }
+                            echo $content;
                             ?>
                         </div>
                         <!--                        <img src="--><?php //echo get_template_directory_uri();?><!--/dist/img/logo-grayscale.png"/>-->
