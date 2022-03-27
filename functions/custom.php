4a9a4b1838a24c1300330ca48a0390f942a9d09e
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2022-03-27 11:13:52 +0530

    chat feature

--- functions/custom.php
+++ functions/custom.php
@@ -181,3 +181,22 @@ function is_local() {
         || substr($_SERVER['HTTP_HOST'],0,7) == '192.168') return true;
     return false;
 }
+
+function chatFeature(){
+    ?>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/623ff8e30bfe3f4a876fe255/1fv4u8f2s';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+<?php
+}
+add_action('wp_footer','chatFeature');
